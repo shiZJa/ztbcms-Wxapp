d11f3a0797011c3b38e42da72f7a9e3935c2da4b
jaren <13428282005@163.com>
2017-06-06 14:56:18 +0800

    小程序 login 时自动注册 cms 账号

--- Controller/BaseController.class.php
+++ Controller/BaseController.class.php
@@ -18,32 +18,7 @@ class BaseController extends Base {
             //没有登录
             $this->ajaxReturn(['status' => false, 'msg' => '未登录', 'code' => "500"]);
         } else {
-
             $this->userInfo = $res['data']['userInfo'];
-            //检查有没有注册
-            $record = M('member')->where(['username' => $this->userInfo['openId']])->find();
-            if (empty($record)) {
-                //未注册
-                $info = [
-                    'username' => $this->userInfo['openId'],
-                    'password' => $this->userInfo['openId'],
-                    'email' => $this->userInfo['openId'] . '@163.com',
-                ];
-
-                $userid = service("Passport")->userRegister($info['username'], $info['password'], $info['email']);
-
-                $data = [
-                    'nickname' => $this->userInfo['nickName'],
-                    'sex' => $this->userInfo['gender'],
-                    'userpic' => $this->userInfo['avatarUrl'],
-                    'regdate' => time(),
-                    'regip' => get_client_ip, //注册的ip地址
-                    'checked' => 1,
-                    //TODO 配置小程序用户模型ID
-//                    'modelid' => 1,
-                ];
-                D('Member')->where("userid='%d'", $userid)->save($data);
-            }
             UserinfoService::updateInfo($this->userInfo);
         }
     }
--- Controller/UserController.class.php
+++ Controller/UserController.class.php
@@ -4,6 +4,7 @@ namespace Wxapp\Controller;
 use Common\Controller\Base;
 use Wxapp\Service\ParseRequestService;
 use Wxapp\Service\WxappService;
+use Wxapp\Lib\AuthAPI;
 
 class UserController extends Base {
     /**
@@ -11,7 +12,42 @@ class UserController extends Base {
      */
     public function login() {
         $wxapp = new WxappService();
-        $wxapp->login();
+        $res = $wxapp->login();
+
+        if ($res['code'] == 0){
+            $id = $res['data']['id'];
+            $skey = $res['data']['skey'];
+
+            $checkResult = AuthAPI::checkLogin($id, $skey);
+
+            $userInfo = $checkResult['user_info'];
+
+            //检查有没有注册
+            $record = M('member')->where(['username' => $this->userInfo['openId']])->find();
+            if (empty($record)) {
+                //未注册
+                $info = [
+                    'username' => $userInfo['openId'],
+                    'password' => $userInfo['openId'],
+                    'email' => $userInfo['openId'] . '@163.com',
+                ];
+
+                //注册 cms 账号
+                $userid = service("Passport")->userRegister($info['username'], $info['password'], $info['email']);
+
+                $data = [
+                    'nickname' => $userInfo['nickName'],
+                    'sex' => $userInfo['gender'],
+                    'userpic' => $userInfo['avatarUrl'],
+                    'regdate' => time(),
+                    'regip' => get_client_ip, //注册的ip地址
+                    'checked' => 1,
+                    // TODO 配置小程序用户模型ID
+//                    'modelid' => 1,
+                ];
+                D('Member')->where("userid='%d'", $userid)->save($data);
+            }
+        }
     }
 
     /**
