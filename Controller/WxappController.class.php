2410421198e8c56131863d6e65295e1b40909e37
zhlhuang <364626853@qq.com>
2017-06-13 19:26:10 +0800

    整理后台配置样式，删除没有必要的聊天室模块

--- Controller/WxappController.class.php
+++ Controller/WxappController.class.php
@@ -7,49 +7,39 @@ use Common\Controller\AdminBase;
  * 微信后台类
  *
  * Class WxappController
+ *
  * @package Wxapp\Controller
  */
-class WxappController extends AdminBase{
+class WxappController extends AdminBase {
 
     /**
      * 展示设置页面
      */
-    function setting(){
+    function setting() {
         $this->display();
     }
 
     /**
      * 获取微信小程序参数
      */
-    function getSettings(){
+    function getSettings() {
         //获取表字段
-        $tablename = C('DB_PREFIX') . "wxapp_appinfo";
         $Model = M('wxappAppinfo');
-        $fields = $Model->query("show full fields from $tablename");
 
         //获取记录
         $setting = $Model->find();
-
-        //关联字段和值
-        foreach ($fields as $k => $v){
-            $v['value'] = $setting[$v['field']];
-            $fields[$k] = $v;
-        }
-
-        $this->ajaxReturn(createReturn(true,$fields));
+        $this->ajaxReturn(createReturn(true, $setting));
     }
 
     /**
      * 设置微信小程序参数
      */
-    function doSetting(){
+    function doSetting() {
         $Model = M('wxappAppinfo');
-
         $result = $Model->data(I('post.'))->save();
-
-        if($result){
+        if ($result) {
             $this->success("设置更新成功");
-        }else {
+        } else {
             $this->error("没有更新或者更新失败");
         }
     }
--- View/Wxapp/setting.php
+++ View/Wxapp/setting.php
@@ -1,30 +1,49 @@
-<?php if (!defined('CMS_VERSION')) exit(); ?>
+<?php if (!defined('CMS_VERSION')) {
+    exit();
+} ?>
 <Admintemplate file="Common/Head"/>
 <body class="J_scroll_fixed">
 <div class="wrap J_check_wrap">
     <Admintemplate file="Common/Nav"/>
-    <div >
+    <div>
         <div class="h_a">配置</div>
         <div id="app" class="hidden">
             <form action="{:U('Wxapp/doSetting')}" method="post">
                 <div class="table_full">
                     <table class="table_form" width="100%" cellspacing="0">
-                        <tbody>
+                        <thead>
                         <tr>
                             <th style="width: 80px;">属性</th>
                             <th style="width: 20%;" colspan="2">当前值</th>
                         </tr>
-                        <tr v-for="setting in settings" :setting="setting">
-                            <td>{{ setting.field }}</td>
-                            <td><input class="form-control" type="text" :name="setting.field" v-model="setting.value"></td>
-                            <td>{{ setting.comment }}</td>
+                        </thead>
+                        <tbody>
+                        <input type="hidden" name="id" v-model="settings.id">
+                        <tr>
+                            <td>appid</td>
+                            <td><input class="form-control" type="text" name="appid" v-model="settings.appid">
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>secret</td>
+                            <td><input class="form-control" type="text" name="secret" v-model="settings.secret">
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>mch_id</td>
+                            <td><input class="form-control" type="text" name="mch_id" v-model="settings.mch_id">
+                            <td>微信支付商户号</td>
+                        </tr>
+                        <tr>
+                            <td>key</td>
+                            <td><input class="form-control" type="text" name="key" v-model="settings.key">
+                            <td>微信支付秘钥</td>
                         </tr>
                         </tbody>
                     </table>
                 </div>
-
                 <div class="form-group">
-                    <button class="btn btn-info" >设置</button>
+                    <button class="btn btn-info">设置</button>
                 </div>
 
             </form>
@@ -39,11 +58,11 @@
     new Vue({
         el: "#app",
         data: {
-            settings: []
+            settings: {}
         },
         methods: {
             getSettings: function () {
-                let that = this;
+                var that = this;
                 $.get('{:U("Wxapp/getSettings")}', {}, function (res) {
                     if (res.status) {
                         that.settings = res.data;
