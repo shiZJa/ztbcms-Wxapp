305900cfb106ee72b35f821a25b5d5b0a2837486
zhlhuang <364626853@qq.com>
2017-06-13 12:14:30 +0800

    获取微信支付预配置

--- Service/WxpayService.class.php
+++ Service/WxpayService.class.php
@@ -5,17 +5,55 @@ use Shop\Service\BaseService;
 use Wxapp\Helper\Util;
 use Wxapp\Lib\HttpUtil;
 
+/**
+ * Class WxpayService
+ *
+ * @package Wxapp\Service
+ */
 class WxpayService extends BaseService {
+    /**
+     * 获取微信支付的前端调用配置
+     *
+     * @param        $openid       用户openid
+     * @param        $out_trade_no 订单交易单号
+     * @param        $total_fee    订单价格，单位分
+     * @param        $notify_url   回调地址
+     * @param string $body         交易简介
+     * @return array
+     */
+    static function getWxpayConfig($openid, $out_trade_no, $total_fee, $notify_url, $body = '小程序微信支付') {
+        $order_res = self::createOrder($openid, $out_trade_no, $total_fee, $notify_url, $body);
+        if ($order_res['status']) {
+            $app = new CappinfoService();
+            $appInfo = $app->select_cappinfo();
+            $prepay_id = $order_res['data'];
+            $data = [
+                'appId' => $appInfo['appid'],
+                'timeStamp' => time(),
+                'nonceStr' => md5(time()),
+                'package' => 'prepay_id=' . $prepay_id,
+                'signType' => 'MD5'
+            ];
+            $data['paySign'] = Util::sign($data, $appInfo['key']);
+
+            return self::createReturn(true, $data, 'ok');
+        } else {
+            return $order_res;
+        }
+    }
+
     /**
      * 创建微信支付预支付订单
      *
      * @param        $openid       用户openid
      * @param        $out_trade_no 订单交易单号
      * @param        $total_fee    订单价格，单位分
-     * @param string $body         交易简介
+     * @param        $notify_url   回调地址
+     * @param string $body
+     *                             交易简介
      * @return array
      */
-    static function createOrder($openid, $out_trade_no, $total_fee, $body = '小程序微信支付') {
+    static function createOrder($openid, $out_trade_no, $total_fee, $notify_url, $body = '小程序微信支付') {
         $app = new CappinfoService();
         $appInfo = $app->select_cappinfo();
         $data = [
@@ -26,7 +64,7 @@ class WxpayService extends BaseService {
             'out_trade_no' => $out_trade_no,
             'total_fee' => $total_fee,
             'spbill_create_ip' => get_client_ip(),
-            'notify_url' => U('index'),
+            'notify_url' => $notify_url,
             'trade_type' => 'JSAPI',
             'openid' => $openid,
         ];
